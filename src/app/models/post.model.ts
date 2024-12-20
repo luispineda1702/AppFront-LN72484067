5dcea99dbce921736244cbc2e360386265f6c94a
luispineda1702 <ln72484067@gmail.com>
2024-12-20 14:59:04 -0500

    antes de las 3

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,8 @@
+//https://jsonplaceholder.typicode.com/posts
+
+export interface PostResponse{
+    userId:string
+    id: string,
+    title: string,
+    body:string
+}
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,12 @@
+//https://jsonplaceholder.typicode.com/users
+
+export interface UserResponse{
+    id: boolean,
+    name: string,
+    username: string,
+    email: string,
+    address: string
+    phone:string,
+    website:string,
+    company:string, 
+}
